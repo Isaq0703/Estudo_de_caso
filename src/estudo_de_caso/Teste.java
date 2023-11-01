2a6a5c7690c67b3ad1ecd8d6978929a1b45a6ce0
LuccaFSAF <lucca.fsa05@aluno.ifsc.edu.br>
2023-11-01 16:14:25 -0300

    teste

---
+++ src/estudo_de_caso/Teste.java
@@ -0,0 +1,17 @@
+package estudo_de_caso;
+
+import javax.swing.JOptionPane;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// so exib mensagem
+		
+		String b = JOptionPane.showInputDialog("Digite algo");
+		System.out.println(b);
+		JOptionPane.showMessageDialog(null, b);
+		
+
+	}
+
+}
